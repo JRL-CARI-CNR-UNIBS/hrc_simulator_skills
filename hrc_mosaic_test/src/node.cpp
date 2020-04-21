22628c776596b82baf2ba38aaebe79e11ba49a78
ManuelBeschi <manuel.beschi@gmail.com>
2020-04-21 11:41:39 +0200

    manuel

--- hrc_mosaic_test/src/node.cpp
+++ hrc_mosaic_test/src/node.cpp
@@ -35,38 +35,60 @@ int main(int argc, char **argv)
     ROS_ERROR("Node %s has not a parameter named group_name",pnh.getNamespace().c_str());
     return -1;
   }
-
+  std::map<std::string,bool> done_recipe;
   for (const std::pair<std::string,std::string>& object: recipe)
   {
-    manipulation_msgs::PickObjectsGoal pick_goal;
-    ROS_INFO("Goal: pick object %s and place it in slot %s",object.second.c_str(),object.first.c_str());
-
-    pick_goal.object_types.push_back(object.second);
-
-    pick_ac.sendGoalAndWait(pick_goal);
-
-
-    if (pick_ac.getResult()->result<0)
-    {
-      ROS_ERROR("unable to pick -> object type =%s",object.second.c_str());
-      continue;
-    }
-    ROS_INFO("well done! I picked it, id=%s",pick_ac.getResult()->object_id.c_str());
-
-    manipulation_msgs::PlaceObjectsGoal place_goal;
-    place_goal.object_type=pick_ac.getResult()->object_type;
-    place_goal.object_id=pick_ac.getResult()->object_id;
-
-    place_goal.place_id=object.first;
-    place_ac.sendGoalAndWait(place_goal);
-
-    if (place_ac.getResult()->result<0)
-    {
-      ROS_ERROR("unable to place");
-      return 0;
-    }
-
+    done_recipe.insert(std::pair<std::string,bool>(object.first,false));
   }
-  ROS_INFO("pick client stopped");
+
+  for (unsigned int pick_itrial=0;pick_itrial<10;pick_itrial++)
+  {
+    bool all_done=true;
+    for (const std::pair<std::string,std::string>& object: recipe)
+    {
+      if (done_recipe.at(object.first))
+        continue;
+
+      manipulation_msgs::PickObjectsGoal pick_goal;
+      ROS_INFO("[Group %s] Goal: pick object %s and place it in slot %s",pnh.getNamespace().c_str(),object.second.c_str(),object.first.c_str());
+
+      pick_goal.object_types.push_back(object.second);
+
+      pick_ac.sendGoalAndWait(pick_goal);
+
+
+      if (pick_ac.getResult()->result<0)
+      {
+        ROS_ERROR("[Group %s] unable to pick -> object type =%s",pnh.getNamespace().c_str(),object.second.c_str());
+        all_done=false;
+        continue;
+      }
+      ROS_INFO("[Group %s] well done! I picked it, id=%s",pnh.getNamespace().c_str(),pick_ac.getResult()->object_id.c_str());
+      done_recipe.at(object.first)=true;
+
+      manipulation_msgs::PlaceObjectsGoal place_goal;
+      place_goal.object_type=pick_ac.getResult()->object_type;
+      place_goal.object_id=pick_ac.getResult()->object_id;
+
+      place_goal.place_id=object.first;
+      place_ac.sendGoalAndWait(place_goal);
+
+      if (place_ac.getResult()->result<0)
+      {
+
+        ROS_ERROR("[Group %s] unable to place, stop it",pnh.getNamespace().c_str());
+        return 0;
+
+
+      }
+
+
+    }
+    if (all_done)
+      break;
+  }
+
+
+  ROS_INFO("[Group %s] pick client stopped",pnh.getNamespace().c_str());
   return 0;
 }
